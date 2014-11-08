aaeb68f1dfcab10b145bc5af3e5424426e72b4b6
Phil Newton <phil@sodaware.net>
2014-11-08 12:26:02 -0500

    Initial commit of Cask file and library

---
+++ teamwork-api.el
@@ -0,0 +1,34 @@
+;;; teamwork-api.el --- Library for working with teamwork.com
+
+;; Copyright (C) 2014 Phil Newton
+
+;; Author: Phil Newton <phil@sodaware.net>
+;; Version: 0.1.0
+;; Package-Requires: ((json "1.2"))
+;; Keywords: teamwork
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; For more information on the API, see the teamwork developer site:
+;; http://developer.teamwork.com/introduction
+
+;;; Code:
+
+;; Dependencies
+
+
+(provide 'teamwork-api)
+;;; teamwork-api.el ends here
